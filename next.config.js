76d277803fd994e7378a3cdbc20db6e7cf13e02b
idriskelani <idmega2000@gmail.com>
2022-12-13 18:42:56 +0100

    refactor portfolio app code

--- next.config.js
+++ next.config.js
@@ -4,22 +4,25 @@ const withImages = require('next-images');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withCSS = require('@zeit/next-css');
 
-module.exports = withImages(
-  withCSS({
-    webpack(config) {
-      config.resolve.alias['components'] = path.join(__dirname, 'src/components');
-      config.resolve.alias['constants'] = path.join(__dirname, 'src/constants');
-      config.resolve.alias['public'] = path.join(__dirname, 'public');
-      config.module.rules.push({
-        test: /\.(eot|woff|woff2|ttf|svg|gif)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-          },
+module.exports = withImages({
+  images: {
+    disableStaticImages: true
+  },
+  webpack(config) {
+    config.resolve.alias['components'] = path.join(__dirname, 'src/components');
+    config.resolve.alias['constants'] = path.join(__dirname, 'src/constants');
+    config.resolve.alias['public'] = path.join(__dirname, 'public');
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
         },
-      });
-      return config;
-    },
-  }),
+      },
+    });
+
+    return config;
+  },
+},
 );
